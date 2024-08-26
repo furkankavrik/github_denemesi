d6dcaaec91f0131c65d0d19f7422c4f5fed73842
furkankavrik <furkankavrik@gmail.com>
2024-08-26 22:48:33 +1000

    Optimising code

--- tests/genel.py
+++ tests/genel.py
@@ -1,15 +1,8 @@
-import pytest
-import playwright
-
-from playwright.sync_api import sync_playwright, Playwright
+from playwright.sync_api import sync_playwright
 
 
 def yeni_sayfa():
-        
-    
-    
     playwright = sync_playwright().start()
     browser = playwright.chromium.launch()
     page = browser.new_page()
-
     return  page
